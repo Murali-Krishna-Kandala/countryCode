f60651ae377527a164fb4b65f7978c77298856fd
srikanth-react <17341a0495@gmrit.edu.in>
2022-05-18 16:41:00 +0530

    secondcommit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  fireEvent,
-  render,
-  act,
-  waitFor,
-} from "@testing-library/react";
+import { fireEvent, render, act, waitFor } from "@testing-library/react";
 import App, { Info, Home } from "./App";
 
 import { BrowserRouter } from "react-router-dom";
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import "./App.css";
 
 function App() {
@@ -68,6 +75,7 @@ export const Info = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
   React.useEffect(() => {
     let rx = localStorage.getItem("myd");
 
@@ -90,30 +98,45 @@ export const Info = () => {
   };
   return (
     <div>
-      <table>
-        {count?.map((a: any) => {
-          return (
-            <tr>
-              <td>{a?.capital}</td>
-              <td>{a?.population}</td>
-              <td>{a?.latlng.join(", ")}</td>
-              <td>
-                <img src={a?.flag} alt="flag" height={60} width={60} />
-              </td>
-              <td>
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    Getcap(a?.capital);
-                  }}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Capital</TableCell>
+              <TableCell align="center">Population</TableCell>
+              <TableCell align="center">Lat&Lng</TableCell>
+              <TableCell align="center">Flag</TableCell>
+              <TableCell align="center">WeatherInfo</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {count?.map((a: any) => {
+              return (
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
-                  GetWeather
-                </Button>
-              </td>
-            </tr>
-          );
-        })}
-      </table>
+                  <TableCell align="center">{a?.capital}</TableCell>
+                  <TableCell align="center">{a?.population}</TableCell>
+                  <TableCell align="right">{a?.latlng.join(", ")}</TableCell>
+                  <TableCell align="center">
+                    <img src={a?.flag} alt="flag" height={60} width={60} />
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button
+                      variant="contained"
+                      onClick={() => {
+                        Getcap(a?.capital);
+                      }}
+                    >
+                      GetWeather
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <Modal
         open={open}
         onClose={handleClose}
@@ -126,8 +149,9 @@ export const Info = () => {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
+            width: 650,
+            height: 200,
+            bgcolor: "#35a422",
             border: "2px solid #000",
             boxShadow: 24,
             p: 4,
@@ -137,10 +161,33 @@ export const Info = () => {
             {cap?.capi}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <p>{wea?.temperature}</p>
-            <p>{wea?.precip}</p>
-            <p>{wea?.wind_speed}</p>
-            <img src={wea?.weather_icons} alt="flag" height={60} width={60} />
+            <TableContainer component={Paper}>
+              <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Temperature</TableCell>
+                    <TableCell align="center">Precipitation</TableCell>
+                    <TableCell align="center">WindSpeed</TableCell>
+                    <TableCell align="center">WeatherIcon</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell align="center">{wea?.temperature}</TableCell>
+                    <TableCell align="center">{wea?.precip}</TableCell>
+                    <TableCell align="center">{wea?.wind_speed}</TableCell>
+                    <TableCell align="center">
+                      <img
+                        src={wea?.weather_icons}
+                        alt="flag"
+                        height={60}
+                        width={60}
+                      />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Typography>
         </Box>
       </Modal>
